122c967c44d9975e67398f2f5d0be1dcd705c749
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-01-21 17:44:40 +0600

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import Home from './Pages/Home';
 export default function App() {
     return (
-        <>
-            <h1 className='text-blue-400 text-9xl'>Test</h1>
-        </>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
     );
 }
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import { MdLocalHospital } from "react-icons/md";
+export default function Logo() {
+    return (
+        <div>
+            <h1 className="flex items-center "> <MdLocalHospital className="mr-1 h-10 w-10 text-blue-700" /> <span className="text-2xl font-bold">Cure & Care</span> </h1>
+        </div>
+    )
+}
